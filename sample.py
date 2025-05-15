e77bbace2dbc6a60ed826976bfb09d96ff6f2063
Simon Riou <80064281+simonriou@users.noreply.github.com>
2025-05-15 18:20:38 +0200

    Refactor sampling script to use MinimalDiffusion model and improve structure

--- sample.py
+++ sample.py
@@ -1,16 +1,24 @@
-from model import SimpleDiffusionModel
+from model import MinimalDiffusion
 from diffusion import sample
 from config import *
 import matplotlib.pyplot as plt
 import torchvision
+import torch
 
-model = SimpleDiffusionModel().to(DEVICE)
-model.load_state_dict(torch.load('model_epoch19.pt'))
-model.eval()
+def main():
+    print("Loading model...")
+    model = MinimalDiffusion().to(DEVICE)
+    model.load_state_dict(torch.load('model_epoch20.pth', map_location=DEVICE))
+    model.eval()
 
-samples = sample(model, image_size=IMG_SIZE, n_samples=16)
-samples = (samples + 1) / 2  # to [0, 1]
-grid = torchvision.utils.make_grid(samples, nrow=4)
-plt.imshow(grid.permute(1, 2, 0).cpu().numpy())
-plt.axis('off')
-plt.show()
+    print("Sampling...")
+    samples = sample(model, image_size=IMG_SIZE, n_samples=16)
+    samples = (samples + 1) / 2  # rescale to [0, 1]
+
+    grid = torchvision.utils.make_grid(samples, nrow=4)
+    plt.imshow(grid.permute(1, 2, 0).cpu().numpy())
+    plt.axis('off')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
